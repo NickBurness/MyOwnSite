ca42c24ac7abca0b67187e0905e29170b7cd2ac7
NickBurness <nickburness93@outlook.com>
2020-10-23 23:25:57 +0100

    implement light/dark mode through seperate stylesheets and js function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 
 let header = document.getElementById("header");
 header.style.height = window.innerHeight + "px";
+
+function changeTheme() {
+    const stylesheet = document.getElementById("theme-sheets");
+
+    if (stylesheet.getAttribute("href") == "./themes/dark-theme.css") {
+        stylesheet.href = "./themes/light-theme.css";
+    } else {
+        stylesheet.href = "./themes/dark-theme.css";
+    }
+}
